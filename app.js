dfe861f4f4bbdfca87e1f1ab1ab03a905bd6477d
Binyam <binyam.techan01@gmail.com>
2023-08-29 20:04:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ pass.addEventListener("input", () => {
         msg.style.display = "none";
     }
     if (pass.value.length < 4) {
-        strength.innerHTML = "Weak";
+        strength.innerText = "Weak";
         pass.style.borderColor = "#ff5925";
         msg.style.color = "#ff5925";
     }
     else if (pass.value.length >= 4 && pass.value.length < 8) {
-        strength.innerHTML = "Medium";
+        strength.innerText = "Medium";
         pass.style.borderColor = "yellow";
         msg.style.color = "yellow";
 
     }
     else if (pass.value.length >= 8) {
-        strength.innerHTML = "Strong";
+        strength.innerText = "Strong";
         pass.style.borderColor = "#26d730";
         msg.style.color = "#26d730";
     }
